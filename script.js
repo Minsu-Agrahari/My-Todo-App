6df2a13eba94c1fcf24b324536ed4edc51d2ede5
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-03-18 17:55:15 +0530

    feat: Enter key added & total task count

--- script.js
+++ script.js
@@ -2,32 +2,50 @@
 
 const taskRef = document.getElementById("inputTask");
 const addBtnRef = document.getElementById("addBtnRef");
-
 const listBody = document.getElementById("parentListRef");
 const totalTaskCount = document.getElementById("totalTask");
 
 let totalTask = 0;
 
-addBtnRef.addEventListener('click', ()=>{
+function addTask(){
+
+    // remove white space
+    const value = taskRef.value.trim();
+    
+    // validation
+    if(!value) return;
 
     // Task creation & append
     const newTask = document.createElement("li");
-    newTask.innerText = taskRef.value;
-    listBody.appendChild(newTask);
+    newTask.innerText = value;
 
-    // totalTask-Count increased
-    totalTask ++;
-
-    // created & add delete-btn -- dec.totalTask-Count
+    // created & add delete-btn
     const delBtn = document.createElement("button");
     delBtn.innerText = "Delete";
-    newTask.append(delBtn);
 
     delBtn.addEventListener("click", ()=>{
         newTask.remove();
         totalTask--; 
     });
 
+    newTask.append(delBtn);
+    listBody.appendChild(newTask);
+
+    totalTask ++;
+    updateTotal();
     taskRef.value = "";
+}
+
+function updateTotal(){
+    totalTaskCount.innerText = totalTask;
+}
+// add btn clicked
+addBtnRef.addEventListener('click', addTask);
+
+// Enter Key
+taskRef.addEventListener('keyup', (e) => {
+    if(e.key === "Enter"){
+        addTask();
+    }
 });
 
